a7d98bf32aa1d589724a8f0f7faca6a8b8c20bd3
Ryan M. Hoffman <rmh.software@gmail.com>
2016-12-01 23:38:08 -0500

    add FabOptions OnClickListeners

--- mobile/src/main/java/com/software/rmh/friends/ContactDetails.java
+++ mobile/src/main/java/com/software/rmh/friends/ContactDetails.java
@@ -3,6 +3,8 @@ package com.software.rmh.friends;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
 
 import com.joaquimley.faboptions.FabOptions;
 
@@ -17,6 +19,28 @@ public class ContactDetails extends AppCompatActivity {
 
 		FabOptions fab = (FabOptions) findViewById(R.id.fabOptions);
 		fab.setButtonsMenu(this, R.menu.menu);
+
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				switch(view.getId()){
+					case R.id.menu_call:
+						Toast.makeText(ContactDetails.this, "Pressed Call", Toast.LENGTH_SHORT).show();
+						break;
+					case R.id.menu_text:
+						Toast.makeText(ContactDetails.this, "Pressed Text", Toast.LENGTH_SHORT).show();
+						break;
+					case R.id.menu_star:
+						Toast.makeText(ContactDetails.this, "Pressed Favorite", Toast.LENGTH_SHORT).show();
+						break;
+					case R.id.menu_edit:
+						Toast.makeText(ContactDetails.this, "Pressed Edit", Toast.LENGTH_SHORT).show();
+						break;
+					default:
+						// No default case.
+				}
+			}
+		});
 
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
