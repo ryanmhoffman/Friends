50a02cb2d05afe1fcd3232f8a8fa22c23d483510
Ryan M. Hoffman <rmh.software@gmail.com>
2016-12-02 20:44:41 -0500

    update theme

--- mobile/src/main/java/com/software/rmh/friends/MainActivity.java
+++ mobile/src/main/java/com/software/rmh/friends/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-		retriever = new ContactRetriever(this);
+		//retriever = new ContactRetriever(this);
 
 		makeDummyContacts();
 
