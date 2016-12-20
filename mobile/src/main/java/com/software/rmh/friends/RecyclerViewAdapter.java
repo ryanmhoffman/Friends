9a22fd9e71a3bec1c804f29e274d3728d6603539
Ryan M. Hoffman <rmh.software@gmail.com>
2016-12-19 22:36:58 -0500

    modify theme

--- mobile/src/main/java/com/software/rmh/friends/RecyclerViewAdapter.java
+++ mobile/src/main/java/com/software/rmh/friends/RecyclerViewAdapter.java
@@ -3,15 +3,19 @@ package com.software.rmh.friends;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Random;
 /**
  * Custom Adapter created by Ryan Hoffman on 8/3/16.
  */
@@ -29,11 +33,13 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 	public static class RecyclerViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 		public TextView contactName;
 		public TextView contactNumber;
+		public ImageView contactPhoto;
 		public RecyclerViewHolder(View itemView) {
 			super(itemView);
 			itemView.setOnClickListener(this);
 			contactName = (TextView) itemView.findViewById(R.id.contactName);
 			contactNumber = (TextView) itemView.findViewById(R.id.contactNumber);
+			contactPhoto = (ImageView) itemView.findViewById(R.id.circleView);
 		}
 
 		@Override
@@ -63,6 +69,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 		if(contacts.get(position) != null){
 			holder.contactName.setText(contacts.get(position).getNAME());
 			holder.contactNumber.setText(contacts.get(position).getNUMBER());
+			holder.contactPhoto.setBackground(getRandomColor());
 		}
 	}
 
@@ -72,5 +79,27 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 			return 0;
 		}
 		return contacts.size();
+	}
+
+	private Drawable getRandomColor(){
+		Random r = new Random();
+		int low = 1;
+		int high = 6;
+		int result = r.nextInt(high - low) + low;
+		switch(result){
+			case 1:
+				return ContextCompat.getDrawable(context, R.drawable.shape_circle_green);
+			case 2:
+				return ContextCompat.getDrawable(context, R.drawable.shape_circle_blue);
+			case 3:
+				return ContextCompat.getDrawable(context, R.drawable.shape_circle_red);
+			case 4:
+				return ContextCompat.getDrawable(context, R.drawable.shape_circle_orange);
+			case 5:
+				return ContextCompat.getDrawable(context, R.drawable.shape_circle_yellow);
+			case 6:
+				return ContextCompat.getDrawable(context, R.drawable.shape_circle_purple);
+		}
+		return ContextCompat.getDrawable(context, R.drawable.shape_circle_green);
 	}
 }
